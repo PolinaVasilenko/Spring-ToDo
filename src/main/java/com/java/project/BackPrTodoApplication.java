5dac4c15d7ab880a69ddcc7a157df1e88f28297e
polina <polindrom@memos.tk>
2022-08-15 15:38:14 +0300

    add ssl

---
+++ src/main/java/com/java/project/BackPrTodoApplication.java
@@ -0,0 +1,13 @@
+package com.java.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPrTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPrTodoApplication.class, args);
+    }
+
+}
